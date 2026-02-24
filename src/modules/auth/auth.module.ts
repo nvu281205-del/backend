5d1af0330699c0cca3a1e2b992dbc0b804d481b7
nvu281205-del <nvu281205@gmail.com>
2026-02-24 11:44:51 +0700

    update 1.4

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,15 +6,24 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from 'src/passport/local.stragery';
 import { JwtStrategy } from 'src/passport/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'Ashuna',
-      signOptions: { expiresIn: '1d' },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          expiresIn: config.get<string>('ACCESS_TOKEN_EXPIRES') as any,
+        },
+      }),
     }),
   ],
   exports: [AuthService],
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-floating-promises */
@@ -5,17 +6,19 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { Users } from 'src/entity/users.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
-  ) {}
+      private readonly configService: ConfigService,
+  ) { }
   generateToken(user:Users){
      const payload = { email: user.email, sub: user.id };
     const refresh_token = this.jwtService.sign(payload, {
-      expiresIn: '7d',
+    expiresIn: this.configService.get<string>('REFRESH_TOKEN_EXPIRES') as any,
     });
     this.usersService.updateRefreshToken(user.id, refresh_token);
     return {
@@ -26,7 +29,7 @@ export class AuthService {
   login(user: Users) {
     const payload = { email: user.email, sub: user.id };
     const refresh_token = this.jwtService.sign(payload, {
-      expiresIn: '7d',
+      expiresIn: this.configService.get<string>('REFRESH_TOKEN_EXPIRES') as any,
     });
     this.usersService.updateRefreshToken(user.id, refresh_token);
     return {
@@ -36,7 +39,6 @@ export class AuthService {
     };
   }
   async verifyRefreshToken(refreshToken: string) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const decode = this.jwtService.verify(refreshToken);
     if (decode) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
